340d367dccd196999c929621cee0e812d1c68831
AlexandreNerriere <43983968+AlexandreNerriere@users.noreply.github.com>
2018-11-30 11:10:32 +0100

    programme ping

---
+++ Projet1_Switch_Party/Programmation/Test de ping/test/main.cpp
@@ -0,0 +1,33 @@
+#include <QCoreApplication>
+#include <time.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int result = 1;
+
+    while (result == 1) {
+        result = system("ping 10.16.2.81");
+        if(result == 1)
+        {
+            printf(" \n \n les branchements ne sont pas bons \n \n");
+            sleep(5);
+
+        }
+
+    }
+
+        printf(" \n \n les resultats sont bons \n \n");
+
+
+
+
+
+
+
+
+    return a.exec();
+}
